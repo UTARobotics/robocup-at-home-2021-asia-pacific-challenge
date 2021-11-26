4551f8a1e66db6006506fc9fee5907c06c1304e3
chungpuonn <45068764+chungpuonn@users.noreply.github.com>
2021-11-26 22:17:16 +0800

    Optimize task 1 and task 2 timeframe

--- robocup_hsr_simulator/scripts/task_1.py
+++ robocup_hsr_simulator/scripts/task_1.py
@@ -825,8 +825,8 @@ if __name__ == "__main__":
     # task 2
     t2 = Task_2()
     print("Starting...")
-    forteen_min = rospy.Duration(13*60) #change state earlier
-    five_min = rospy.Duration(6*60)
+    forteen_min = rospy.Duration(12.5*60) #change state earlier
+    five_min = rospy.Duration(7.5*60)
     start = rospy.Time.now()
     while not rospy.is_shutdown():
         try: 
